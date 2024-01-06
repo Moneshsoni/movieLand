e60d3b867d4e2f1d337d2ba426d94ffc99faa5c6
Moneshsoni <sonimonesh115@gmail.com>
2024-01-06 17:40:46 +0530

    make the weakend react projects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import './App.css';
-import Person from  './components/Person';
-import Counter from './components/Counter';
+import React,{useEffect, useState} from 'react';
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from './MovieCard';
 
-function App() {
- 
-  return (
-    <div className="App">
-      <Person name={'Monesh'} l={'soni'} age={123}/>
-      < Counter />
-    </div>
-  );
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=46e960cb';
+const App = ()=>{
+    const [movies, setMovies] = useState([]);
+    const [searchTerm,setSearchTerm]= useState('')
+    const searchMovies = async(title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`) 
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(()=>{
+        searchMovies('Spiderman')
+    },[]);
+
+    return(
+        <div className='app'>
+            <h1>MovieLand</h1>
+            <div className='search'>
+            <input placeholder='Search for movie' value ={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
+                <img src={SearchIcon}
+                alt="search" onClick={()=>searchMovies(searchTerm)}/>
+            </div>
+            {
+                movies?.length>0?(
+                    <div className='container'>
+                        {movies.map((movie)=>(<MovieCard movie = {movie} />))}
+                    </div>
+                ):(
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+        </div> 
+    ) 
 }
-
 export default App;
